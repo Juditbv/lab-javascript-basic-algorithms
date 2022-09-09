99ec1cafa07165ce445f0cf70d116ac9f9af909b
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-09 18:12:30 +0200

    add last iteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 // Iteration 1: Names and Input
-//
+const hacker1 = "Raquel";
+// console.log(`The driver's name is ${hacker1}`);
+
+// const hacker2 = "Raquel";
+// console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
-
+// if (hacker1.length > hacker2.length) {
+//   console.log(
+//     `The driver has the longest name, it has ${hacker1.length} characters.`
+//   );
+// } else if (hacker1.length < hacker2.length) {
+//   console.log(
+//     `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+//   );
+// } else if (hacker1.length === hacker2.length) {
+//   console.log(
+//     `Wow, you both have equally long names, ${hacker1.length} characters!`
+//   );
+// }
 // Iteration 3: Loops
+const nameUpperCase = hacker1.toUpperCase().split(" ");
+let nameWithSpaces = "";
+for (let i = 0; i <= nameUpperCase.length; i++) {
+  let letterSpace = nameUpperCase[i] + " ";
+  nameWithSpaces += letterSpace;
+}
+console.log(nameWithSpaces);
